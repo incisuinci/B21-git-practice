31ef61098de5a845d86021611019e4c88802e636
incisuinci <incisuinci@gmail.com>
2021-01-26 16:57:58 -0500

    everything

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -2,7 +2,8 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World!,B21");
         System.out.println("B21 future SDET !!");
-//ready to go back
+    //ready to go back
+
     }
 
 }
