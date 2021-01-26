578b4b8b774ed05a2b906201df406ed11904ee0b
incisuinci <incisuinci@gmail.com>
2021-01-26 16:29:30 -0500

    move5 new changes

--- src/main/java/SeeingDifference.java
+++ src/main/java/SeeingDifference.java
@@ -2,5 +2,6 @@ public class SeeingDifference {
     public static void main(String[] args) {
         System.out.println("difference");
         //this is new line of work
+        //this is a totaly new change
     }
 }
